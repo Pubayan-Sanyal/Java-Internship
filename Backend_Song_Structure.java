32250ec23efa41b5138d17e34c41d457b13c05e1
Pubayan-Sanyal <98461524+Pubayan-Sanyal@users.noreply.github.com>
2023-07-13 20:06:32 +0530

    The backend structures to be integrated with UI

---
+++ Backend_Song_Structure.java
@@ -0,0 +1,31 @@
+public class Song
+{
+    String title; //title of the song
+    double duration; //duration of the song
+
+    public Song(String title, double duration)
+    {
+        //constructor to initialize
+        this.title = title;
+        this.duration = duration;
+    }
+
+    public String getTitle()
+    {
+        //function to return song title
+        return title;
+    }
+
+    public double getDuration()
+    {
+        //function to return song duration
+        return duration;
+    }
+
+    @Override
+    public String toString()
+    {
+        //Function to return properties of the song
+        return "Song{" + "title : " + title + " , duration : " + duration + "}";
+    }
+}
